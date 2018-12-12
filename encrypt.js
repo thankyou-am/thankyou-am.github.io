63c1e829a2deae06aceb0b78e44ba0a805bf52b7
shimoda.keitaro <shimoda.keitaro@raccoon.ne.jp>
2018-12-12 16:34:49 +0900

    二番煎じ

--- encrypt.js
+++ encrypt.js
@@ -1,4 +1,4 @@
 document.querySelector('#encrypt').onclick = function() {
     document.querySelector('#encrypted').value =
-        ',"' + cryptico.encrypt(encode(document.querySelector('#raw').value), "btxvX3WK050SzT+UoGwvOPf+ZM0qnI8kGYdmHv4ah3uaWvwsCKVEV/Iwm2YNK9T3ZwM2nbKBEdJn34WEHtcBRI90r7CkCQSwtEibseu2+aZakZtcJC4T4mEm5WgqcoymvL9JgJyE8hvnMdKbtnk4qKVaVuixnieSwPybafW04ck=").cipher + '"'
+        ',"' + cryptico.encrypt(encode(document.querySelector('#raw').value), "3e/zzxz2VTYC4kxPfjCrzf1f4H9oh7iPGIp2mWzejIub+T1KLBCuC/xT8JsyF8SkRxDPD2sO53bP9nsZc2mLvoH8mxzJ7Vxu+fFiKrlCNMufBh0KrGq67eEQLmYRsdM4wtT1n2TIXnyB5kievMGdQNnyV/tfqXqIWuFAqomY/RM=").cipher + '"'
 }
